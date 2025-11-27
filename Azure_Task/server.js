531e025813a03df60df824f74c490f8e023e0fb4
Bhuvaneswari-tech <bhuvanagitrepo@gmail.com>
2025-11-27 18:47:00 +0530

    Azure Task

--- Azure_Task/server.js
+++ Azure_Task/server.js
@@ -1,6 +1,6 @@
 // server.js
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3501;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
